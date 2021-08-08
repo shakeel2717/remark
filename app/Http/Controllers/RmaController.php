81eec00b9c99345b08409d96d901bfedf38cb12b
shakeel2717 <shakeel2717@gmail.com>
2021-08-08 12:27:21 -0700

    Supplier Customer name Fixed

--- app/Http/Controllers/RmaController.php
+++ app/Http/Controllers/RmaController.php
@@ -32,24 +32,24 @@ class RmaController extends Controller
 
     public function rmaCustomer()
     {
-        return view('dashboard.rma.index',[
-            'customers' => Customer::where('users_id',session('user')[0]->id)->where('type',0)->get(),
-            'rmas' => Rma::where('users_id',session('user')[0]->id)->where('type',0)->paginate(10),
-            'warehouses' => Warehouse::where('users_id',session('user')[0]->id)->get(),
-            'suppliers' => Customer::where('users_id',session('user')[0]->id)->where('type',1)->get(),
+        return view('dashboard.rma.index', [
+            'customers' => Customer::where('users_id', session('user')[0]->id)->where('type', 0)->get(),
+            'rmas' => Rma::where('users_id', session('user')[0]->id)->where('type', 0)->paginate(10),
+            'warehouses' => Warehouse::where('users_id', session('user')[0]->id)->get(),
+            'suppliers' => Customer::where('users_id', session('user')[0]->id)->where('type', 1)->get(),
         ]);
     }
 
     public function rmaSupplier()
     {
-        return view('dashboard.rma.index',[
-            'customers' => Customer::where('users_id',session('user')[0]->id)->where('type',0)->get(),
-            'rmas' => Rma::where('users_id',session('user')[0]->id)->where('type',1)->paginate(10),
-            'warehouses' => Warehouse::where('users_id',session('user')[0]->id)->get(),
-            'suppliers' => Customer::where('users_id',session('user')[0]->id)->where('type',1)->get(),
+        return view('dashboard.rma.index', [
+            'customers' => Customer::where('users_id', session('user')[0]->id)->where('type', 0)->get(),
+            'rmas' => Rma::where('users_id', session('user')[0]->id)->where('type', 1)->paginate(10),
+            'warehouses' => Warehouse::where('users_id', session('user')[0]->id)->get(),
+            'suppliers' => Customer::where('users_id', session('user')[0]->id)->where('type', 1)->get(),
         ]);
     }
-    
+
 
     /**
      * Show the form for creating a new resource.
@@ -78,7 +78,11 @@ class RmaController extends Controller
         // storing this request into database
         $task = new Rma();
         $task->users_id = session('user')[0]->id;
-        $task->customer_id = $validated['customer'];
+        if ($validated['type'] == 0) {
+            $task->customer_id = $validated['customer'];
+        } elseif ($validated['type'] == 1) {
+            $task->customer_id = $validated['supplier'];
+        }
         $task->warehouse_id = $validated['warehouse'];
         $task->supplire_id = $validated['supplier'];
         $task->status = "New";
@@ -93,7 +97,7 @@ class RmaController extends Controller
         $task->customers_id = $validated['customer'];
         $task->rma_id = $rMAID;
         $task->title = "RMA Created Successfully";
-        $task->value = "RMA# $rMAID Created by ".session('user')[0]->fname." ".session('user')[0]->lname." on ".$rmadate."";
+        $task->value = "RMA# $rMAID Created by " . session('user')[0]->fname . " " . session('user')[0]->lname . " on " . $rmadate . "";
         $task->save();
         return redirect()->back()->with('message', 'RMA was successfully created');
     }
@@ -107,11 +111,11 @@ class RmaController extends Controller
     public function show(Rma $rma)
     {
         // fetching Add Inventory Data
-        $addInventory = addInventory::where(['users_id' => session('user')[0]->id , 'rma_id' => $rma->id])->get();
-        $rmaRefunds = rmaRefunds::where(['users_id' => session('user')[0]->id , 'rma_id' => $rma->id])->get();
+        $addInventory = addInventory::where(['users_id' => session('user')[0]->id, 'rma_id' => $rma->id])->get();
+        $rmaRefunds = rmaRefunds::where(['users_id' => session('user')[0]->id, 'rma_id' => $rma->id])->get();
         $reasons = Reason::where('users_id', session('user')[0]->id)->get();
         $rmaHistory = rmahistory::where('users_id', session('user')[0]->id)->where('rma_id', $rma->id)->get();
-        return view('dashboard.rma.show',[
+        return view('dashboard.rma.show', [
             'rma' => $rma,
             'addInventory' => $addInventory,
             'rmaRefunds' => $rmaRefunds,
